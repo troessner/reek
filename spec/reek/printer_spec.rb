7ca6798d664503444bd92f14c7cfded7378400d3
Kevin Rutherford <kevin@rutherford-software.com>
2008-12-17 15:56:01 +0000

    Avoid helper method name clashes

--- spec/reek/printer_spec.rb
+++ spec/reek/printer_spec.rb
@@ -5,7 +5,7 @@ require 'reek/printer'
 
 include Reek
 
-def check(examples)
+def should_print(examples)
   examples.each do |actual|
     it "should format #{actual} correctly" do
       sexp = CodeParser.parse_tree_for(actual)[0]
@@ -15,17 +15,18 @@ def check(examples)
 end
 
 describe Printer do
-  check 'self'
-  check 'Alpha'
-  check 'Alpha::Beta'
-  check '@@fred'
-  check '`ls`'
-  check 'array[0]'
-  check 'array[0, 1, 2]'
-  check 'obj.method(arg1, arg2)'
-  check 'obj.method'
-  check '$1'
-  check 'o=q.downcase'
-  check 'true'
-  check '"-#{q}xxx#{z.size}"'
+
+  should_print 'self'
+  should_print 'Alpha'
+  should_print 'Alpha::Beta'
+  should_print '@@fred'
+  should_print '`ls`'
+  should_print 'array[0]'
+  should_print 'array[0, 1, 2]'
+  should_print 'obj.method(arg1, arg2)'
+  should_print 'obj.method'
+  should_print '$1'
+  should_print 'o=q.downcase'
+  should_print 'true'
+  should_print '"-#{q}xxx#{z.size}"'
 end
