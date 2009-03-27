082e55302c7081913ded0ee5cd4a3e84d7dc477a
Kevin Rutherford <kevin@rutherford-software.com>
2009-03-27 11:13:16 +0000

    Renamed the spec matcher to reek

--- spec/reek/smells/feature_envy_spec.rb
+++ spec/reek/smells/feature_envy_spec.rb
@@ -14,7 +14,7 @@ class String
   end
 end
 
-class ShouldSmell
+class ShouldReek
   def matches?(actual)
     @source = actual.to_source
     @source.smelly?
@@ -27,11 +27,11 @@ class ShouldSmell
   end
 end
 
-def smell
-  ShouldSmell.new
+def reek
+  ShouldReek.new
 end
 
-class ShouldSmellOf
+class ShouldReekOf
   def initialize(klass, patterns)
     @klass = klass
     @patterns = patterns
@@ -48,37 +48,37 @@ class ShouldSmellOf
   end
 end
 
-def smell_of(klass, *patterns)
-  ShouldSmellOf.new(klass, patterns)
+def reek_of(klass, *patterns)
+  ShouldReekOf.new(klass, patterns)
 end
 
 #-------------------------------------------------------------------------------------------
 
 describe FeatureEnvy, 'with only messages to self' do
   it 'should not report use of self' do
-    'def simple() self.to_s + self.to_i end'.should_not smell
+    'def simple() self.to_s + self.to_i end'.should_not reek
   end
 
   it 'should not report vcall with no argument' do
-    'def simple() func; end'.should_not smell
+    'def simple() func; end'.should_not reek
   end
 
   it 'should not report vcall with argument' do
-    'def simple(arga) func(17); end'.should_not smell
+    'def simple(arga) func(17); end'.should_not reek
   end
 end
 
 describe FeatureEnvy, 'when the receiver is a parameter' do
   it 'should not report single use' do
-    'def no_envy(arga) arga.barg(@item) end'.should_not smell
+    'def no_envy(arga) arga.barg(@item) end'.should_not reek
   end
 
   it 'should not report return value' do
-    'def no_envy(arga) arga.barg(@item); arga end'.should_not smell
+    'def no_envy(arga) arga.barg(@item); arga end'.should_not reek
   end
 
   it 'should report many calls to parameter' do
-    'def envy(arga) arga.b(arga) + arga.c(@fred) end'.should smell_of(:FeatureEnvy, /arga/)
+    'def envy(arga) arga.b(arga) + arga.c(@fred) end'.should reek_of(:FeatureEnvy, /arga/)
   end
 end
 
@@ -91,8 +91,8 @@ describe FeatureEnvy, 'when there are many possible receivers' do
       total += fred.tax
       total *= 1.15
     end'
-    ruby.should smell_of(:FeatureEnvy, /total/)
-    ruby.should_not smell_of(:FeatureEnvy, /fred/)
+    ruby.should reek_of(:FeatureEnvy, /total/)
+    ruby.should_not reek_of(:FeatureEnvy, /fred/)
   end
 
   it 'should report multiple affinities' do
@@ -102,39 +102,39 @@ describe FeatureEnvy, 'when there are many possible receivers' do
       total += fred.price
       total += fred.tax
     end'
-    ruby.should smell_of(:FeatureEnvy, /total/)
-    ruby.should smell_of(:FeatureEnvy, /fred/)
+    ruby.should reek_of(:FeatureEnvy, /total/)
+    ruby.should reek_of(:FeatureEnvy, /fred/)
   end
 end
 
 describe FeatureEnvy, 'when the receiver is external' do
   it 'should ignore global variables' do
-    'def no_envy() $s2.to_a; $s2[@item] end'.should_not smell
+    'def no_envy() $s2.to_a; $s2[@item] end'.should_not reek
   end
 
   it 'should not report class methods' do
-    'def simple() self.class.new.flatten_merge(self) end'.should_not smell
+    'def simple() self.class.new.flatten_merge(self) end'.should_not reek
   end
 end
 
 describe FeatureEnvy, 'when the receiver is an ivar' do
   it 'should not report single use of an ivar' do
-    'def no_envy() @item.to_a end'.should_not smell
+    'def no_envy() @item.to_a end'.should_not reek
   end
 
   it 'should not report returning an ivar' do
-    'def no_envy() @item.to_a; @item end'.should_not smell
+    'def no_envy() @item.to_a; @item end'.should_not reek
   end
 
   it 'should not report ivar usage in a parameter' do
-    'def no_envy; @item.price + tax(@item) - savings(@item) end'.should_not smell
+    'def no_envy; @item.price + tax(@item) - savings(@item) end'.should_not reek
   end
 
   it 'should not be fooled by duplication' do
-    'def feed(thing); @cow.feed_to(thing.pig); @duck.feed_to(thing.pig); end'.should_not smell_of(:FeatureEnvy)
+    'def feed(thing); @cow.feed_to(thing.pig); @duck.feed_to(thing.pig); end'.should_not reek_of(:FeatureEnvy)
   end
   it 'should count local calls' do
-    'def feed(thing); cow.feed_to(thing.pig); duck.feed_to(thing.pig); end'.should_not smell_of(:FeatureEnvy)
+    'def feed(thing); cow.feed_to(thing.pig); duck.feed_to(thing.pig); end'.should_not reek_of(:FeatureEnvy)
   end
 end
 
@@ -158,18 +158,18 @@ end
 
 describe FeatureEnvy, 'when the receiver is an lvar' do
   it 'should not report single use of an lvar' do
-    'def no_envy() lv = @item; lv.to_a end'.should_not smell
+    'def no_envy() lv = @item; lv.to_a end'.should_not reek
   end
 
   it 'should not report returning an lvar' do
-    'def no_envy() lv = @item; lv.to_a; lv end'.should_not smell
+    'def no_envy() lv = @item; lv.to_a; lv end'.should_not reek
   end
 
   it 'should report many calls to lvar' do
-    'def envy; lv = @item; lv.price + lv.tax end'.should smell_of(:FeatureEnvy, /lv/)
+    'def envy; lv = @item; lv.price + lv.tax end'.should reek_of(:FeatureEnvy, /lv/)
   end
 
   it 'should not report lvar usage in a parameter' do
-    'def no_envy; lv = @item; lv.price + tax(lv) - savings(lv) end'.should_not smell
+    'def no_envy; lv = @item; lv.price + tax(lv) - savings(lv) end'.should_not reek
   end
 end
