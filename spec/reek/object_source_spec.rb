e1767a30e89c6f010998c96d8aa9bcd0b344dcdb
Kevin Rutherford <kevin@rutherford-software.com>
2009-06-30 22:01:26 +0100

    Added tests for crazy edges cases for Dir matcher

--- spec/reek/object_source_spec.rb
+++ spec/reek/object_source_spec.rb
@@ -4,11 +4,20 @@ include Reek
 
 describe Dir do
   it 'reports correct smells via the Dir matcher' do
+    Dir['spec/samples/two_smelly_files/*.rb'].should reek
     Dir['spec/samples/two_smelly_files/*.rb'].should reek_of(:UncommunicativeName)
   end
 
   it 'reports correct smells via SourceList' do
     src = Dir['spec/samples/two_smelly_files/*.rb'].to_source
     src.has_smell?(:UncommunicativeName).should be_true
+  end
+
+  it 'copes with daft file specs' do
+    Dir["spec/samples/two_smelly_files/*/.rb"].should_not reek
+  end
+
+  it 'copes with empty array' do
+    [].should_not reek
   end
 end
