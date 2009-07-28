de6096d1434f258d8de02e28b01af391b8a49a59
Kevin Rutherford <kevin@rutherford-software.com>
2009-07-28 20:36:10 +0100

    Basic rdoc for SimulatedPolymorphism

--- lib/reek/smells/simulated_polymorphism.rb
+++ lib/reek/smells/simulated_polymorphism.rb
@@ -6,7 +6,21 @@ module Reek
   module Smells
 
     #
-    # TBS
+    # Simulated Polymorphism occurs when
+    # * code uses a case statement (especially on a type field);
+    # * or code has several if statements in a row (especially if they're comparing against the same value);
+    # * or code uses instance_of?, kind_of?, is_a?, or === to decide what type it's working with;
+    # * or multiple conditionals in different places test the same value.
+    #
+    # Conditional code is hard to read and understand, because the reader must
+    # hold more state in his head.
+    # When the same value is tested in multiple places throughout an application,
+    # any change to the set of possible values will require many methods and
+    # classes to change. Tests for the type of an object may indicate that the
+    # abstraction represented by that type is not completely defined (or understood).
+    #
+    # In the current implementation, Reek only checks for multiple conditionals
+    # testing the same value throughout a single class.
     #
     class SimulatedPolymorphism < SmellDetector
 
@@ -14,16 +28,28 @@ module Reek
         [:class]
       end
 
+      # The name of the config field that sets the maximum number of
+      # identical conditionals permitted within any single class.
+      MAX_IDENTICAL_IFS_KEY = 'max_ifs'
+
+      def self.default_config
+        super.adopt(MAX_IDENTICAL_IFS_KEY => 2)
+      end
+
+      def initialize(config = SimulatedPolymorphism.default_config)
+        super(config)
+      end
+
       #
-      # Checks ... TBS
+      # Checks the given ClassContext for multiple identical conditional tests.
       # Remembers any smells found.
       #
       def examine_context(klass)
         counts = Hash.new(0)
         klass.conditionals.each {|cond| counts[cond] += 1}
-        counts.each {|key, val|
-          found(klass, "tests #{SexpFormatter.format(key)} at least #{val} times") if val >= 3
-        }
+        counts.each do |key, val|
+          found(klass, "tests #{SexpFormatter.format(key)} at least #{val} times") if val > @config[MAX_IDENTICAL_IFS_KEY]
+        end
       end
     end
   end
