6d98f44cf9ea967e819e5c3f0553fadc0db64561
Kevin Rutherford <kevin@rutherford-software.com>
2009-10-05 21:47:40 +0100

    Created some shared specs

--- spec/reek/smells/attribute_spec.rb
+++ spec/reek/smells/attribute_spec.rb
@@ -3,79 +3,24 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 require 'reek/smells/attribute'
 require 'reek/class_context'
 
+require 'spec/reek/smells/behaves_like_variable_detector'
+
 include Reek
 include Reek::Smells
 
 describe Attribute do
-  shared_examples_for 'an attribute detector' do
-    context 'with no attributes' do
-      it "doesn't record a smell" do
-        @detector.examine_context(@ctx)
-        @detector.num_smells.should == 0
-      end
-    end
-
-    context 'with one attribute' do
-      before :each do
-        @ctx.record_attribute(:property)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records a smell' do
-        @detector.num_smells.should == 1
-      end
-      it 'mentions the variable name in the report' do
-        @detector.should have_smell([/property/])
-      end
-    end
-
-    context 'with one attribute encountered twice' do
-      before :each do
-        @ctx.record_attribute(:property)
-        @ctx.record_attribute(:property)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records only one smell' do
-        @detector.num_smells.should == 1
-      end
-      it 'mentions the variable name in the report' do
-        @detector.should have_smell([/property/])
-      end
-    end
-
-    context 'with two attributes' do
-      before :each do
-        @ctx.record_attribute(:property)
-        @ctx.record_attribute(:another)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records a smell' do
-        @detector.num_smells.should == 2
-      end
-      it 'mentions both variable names in the report' do
-        @detector.should have_smell([/property/])
-        @detector.should have_smell([/another/])
-      end
-    end
+  before :each do
+    @detector = Attribute.new
+    @record_variable = :record_attribute
   end
 
-  context 'in a class' do
-    before :each do
-      @ctx = ClassContext.create(StopContext.new, "Fred")
-      @detector = Attribute.new
+  [ClassContext, ModuleContext].each do |klass|
+    context "in a #{klass}" do
+      before :each do
+        @ctx = klass.create(StopContext.new, "Fred")
+      end
+
+      it_should_behave_like 'a variable detector'
     end
-
-    it_should_behave_like 'an attribute detector'
-  end
-
-  context 'in a module' do
-    before :each do
-      @ctx = ModuleContext.create(StopContext.new, "Fred")
-      @detector = Attribute.new
-    end
-
-    it_should_behave_like 'an attribute detector'
   end
 end
--- spec/reek/smells/class_variable_spec.rb
+++ spec/reek/smells/class_variable_spec.rb
@@ -3,79 +3,24 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 require 'reek/smells/class_variable'
 require 'reek/class_context'
 
+require 'spec/reek/smells/behaves_like_variable_detector'
+
 include Reek
 include Reek::Smells
 
 describe ClassVariable do
-  shared_examples_for 'a class variable container' do
-    context 'with no class variables' do
-      it "doesn't record a smell" do
-        @detector.examine_context(@ctx)
-        @detector.num_smells.should == 0
-      end
-    end
-
-    context 'with one class variable' do
-      before :each do
-        @ctx.record_class_variable(:@@cvar)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records a smell' do
-        @detector.num_smells.should == 1
-      end
-      it 'mentions the variable name in the report' do
-        @detector.should have_smell([/@@cvar/])
-      end
-    end
-
-    context 'with one class variable encountered twice' do
-      before :each do
-        @ctx.record_class_variable(:@@cvar)
-        @ctx.record_class_variable(:@@cvar)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records only one smell' do
-        @detector.num_smells.should == 1
-      end
-      it 'mentions the variable name in the report' do
-        @detector.should have_smell([/@@cvar/])
-      end
-    end
-
-    context 'with two class variables' do
-      before :each do
-        @ctx.record_class_variable(:@@cvar)
-        @ctx.record_class_variable(:@@another)
-        @detector.examine_context(@ctx)
-      end
-
-      it 'records a smell' do
-        @detector.num_smells.should == 2
-      end
-      it 'mentions both variable names in the report' do
-        @detector.should have_smell([/@@cvar/])
-        @detector.should have_smell([/@@another/])
-      end
-    end
+  before :each do
+    @detector = ClassVariable.new
+    @record_variable = :record_class_variable
   end
 
-  context 'in a class' do
-    before :each do
-      @ctx = ClassContext.create(StopContext.new, "Fred")
-      @detector = ClassVariable.new
+  [ClassContext, ModuleContext].each do |klass|
+    context "in a #{klass}" do
+      before :each do
+        @ctx = klass.create(StopContext.new, "Fred")
+      end
+
+      it_should_behave_like 'a variable detector'
     end
-
-    it_should_behave_like 'a class variable container'
-  end
-
-  context 'in a module' do
-    before :each do
-      @ctx = ModuleContext.create(StopContext.new, "Fred")
-      @detector = ClassVariable.new
-    end
-
-    it_should_behave_like 'a class variable container'
   end
 end
