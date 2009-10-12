81a021c9db34a38d464903072975720bd5cb805e
Kevin Rutherford <kevin@rutherford-software.com>
2009-10-12 14:47:36 +0100

    First feature for RakeTask

--- Rakefile
+++ Rakefile
@@ -1,4 +1,3 @@
-require 'rake'
 require 'rake/clean'
 
 $:.unshift File.dirname(__FILE__) + '/lib'
--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@ require 'reek/adapters/application'
 
 class ReekWorld
   def run(cmd)
-    stderr_file = Tempfile.new('cucumber')
+    stderr_file = Tempfile.new('reek-world')
     stderr_file.close
     @last_stdout = `#{cmd} 2> #{stderr_file.path}`
     @last_exit_status = $?.exitstatus
@@ -24,8 +24,20 @@ class ReekWorld
     run("echo \"#{stdin}\" | ruby -Ilib bin/reek #{args}")
   end
 
-  def rake
-    run("rake reek")
+  def rake(task_def)
+    header = <<EOS
+$:.unshift('lib')
+require 'reek/adapters/rake_task'
+
+EOS
+    rakefile = Tempfile.new('rake_task', '.')
+    rakefile.puts(header + task_def)
+    rakefile.close
+    run("rake -f #{rakefile.path} reek")
+    lines = @last_stdout.split("\n")
+    if lines.length > 0 and lines[0] =~ /^\(/
+      @last_stdout = lines[1..-1].join("\n")
+    end
   end
 end
 
