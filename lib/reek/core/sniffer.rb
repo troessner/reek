ff7555c41640b2dc18429d3071ad820484252274
Andrew Wagner <wagner.andrew@gmail.com>
2012-02-25 21:31:39 -0500

    move hash extensions into separate file

--- lib/reek/core/sniffer.rb
+++ lib/reek/core/sniffer.rb
@@ -2,35 +2,7 @@ require File.join(File.dirname(File.expand_path(__FILE__)), 'code_parser')
 require File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'smells')
 require File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'source', 'config_file')
 require 'yaml'
-
-#
-# Extensions to +Hash+ needed by Reek.
-#
-class Hash
-  def push_keys(hash)
-    keys.each {|key| hash[key].adopt!(self[key]) }
-  end
-
-  def adopt!(other)
-    other.keys.each do |key|
-      ov = other[key]
-      if Array === ov and has_key?(key)
-        self[key] += ov
-      else
-        self[key] = ov
-      end
-    end
-    self
-  end
-  
-  def adopt(other)
-    self.deep_copy.adopt!(other)
-  end
-  
-  def deep_copy
-    YAML::load(YAML::dump(self))
-  end
-end
+require File.join(File.dirname(File.expand_path(__FILE__)), 'hash_extensions')
 
 module Reek
   module Core
