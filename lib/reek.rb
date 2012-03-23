87d10a9c0ede1020c02e963d0193e144ce625a06
Andrew Wagner <wagner.andrew@gmail.com>
2012-03-23 13:33:48 -0400

    Revert "Revert "Merge pull request #97 from mvz/modern-gemspec""
    This reverts commit 876daf4f5c38ad6f125485f7e8aea397ecfbcf46.

--- lib/reek.rb
+++ lib/reek.rb
@@ -1,9 +1,6 @@
 #
 # Reek's core functionality
 #
-module Reek
-  VERSION = '1.2.8'
-end
-
+require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'version')
 require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'examiner')
 require File.join(File.dirname(File.expand_path(__FILE__)), 'reek', 'smell_warning')
