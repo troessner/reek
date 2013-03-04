1f08417b94f011ebcb6d3ea650bcede713b2dd17
snusnu <gamsnjaga@gmail.com>
2013-03-04 01:05:40 +0100

    Extract private methods in Smells::UnusedParameters

--- lib/reek/smells/unused_parameters.rb
+++ lib/reek/smells/unused_parameters.rb
@@ -20,16 +20,37 @@ module Reek
       # @return [Array<SmellWarning>]
       #
       def examine_context(method_ctx)
-        params = method_ctx.exp.arg_names || []
-        params.select do |param|
-          param = param.to_s.sub(/^\*/, '')
-          !["", "_"].include?(param) &&
-            !method_ctx.local_nodes(:lvar).include?(Sexp.new(:lvar, param.to_sym))
-        end.map do |param|
+        unused_params(method_ctx).map do |param|
           SmellWarning.new(SMELL_CLASS, method_ctx.full_name, [method_ctx.exp.line],
                            "has unused parameter '#{param.to_s}'",
                            @source, SMELL_SUBCLASS, {PARAMETER_KEY => param.to_s})
         end
+      end
+
+      private
+
+      def unused_params(method_ctx)
+        params(method_ctx).select do |param|
+          param = sanitized_param(param)
+          next if marked_unused?(param)
+          unused?(method_ctx, param)
+        end
+      end
+
+      def marked_unused?(param)
+        ["", "_"].include?(param)
+      end
+
+      def unused?(method_ctx, param)
+        !method_ctx.local_nodes(:lvar).include?(Sexp.new(:lvar, param.to_sym))
+      end
+
+      def params(method_ctx)
+        method_ctx.exp.arg_names || []
+      end
+
+      def sanitized_param(param)
+        param.to_s.sub(/^\*/, '')
       end
 
     end
