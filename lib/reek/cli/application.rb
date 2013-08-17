d8e7acab44e1c78fa49bf7ebd617bf62d9bb8afe
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-08-16 22:04:09 -0400

    corrected pluralization of 0 total warnings

--- lib/reek/cli/application.rb
+++ lib/reek/cli/application.rb
@@ -44,7 +44,7 @@ module Reek
 
       def output_smells_total(total_smells_count)
         total_smells_message = "#{total_smells_count} total warning"
-        total_smells_message += 's' unless total_smells_count <= 1  
+        total_smells_message += 's' unless total_smells_count == 1  
         total_smells_message += "\n"
         output total_smells_message 
       end
