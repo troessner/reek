40d586828ccc9cf3ac00df436a7b1a046d880497
Alexander Simonov <alex@simonov.me>
2014-07-05 23:44:51 +0300

    Do not warn smell PrimaDonnaMethod if attr_accessor or attr_reader is exist

--- lib/reek/smells/prima_donna_method.rb
+++ lib/reek/smells/prima_donna_method.rb
@@ -20,7 +20,7 @@ module Reek
     # ------------------------------
     # Such a method is called PrimaDonnaMethod and is reported as a smell.
     class PrimaDonnaMethod < SmellDetector
-      SMELL_CLASS    = smell_class_name
+      SMELL_CLASS = smell_class_name
       SMELL_SUBCLASS = smell_class_name
 
       def self.contexts
@@ -29,12 +29,32 @@ module Reek
 
       def examine_context(ctx)
         ctx.node_instance_methods.map do |method_sexp|
-          if method_sexp.ends_with_bang?
-            SmellWarning.new(SMELL_CLASS, ctx.full_name, [ctx.exp.line],
-                             %Q!has prima donna method `#{method_sexp.name}`!,
-                             @source, SMELL_SUBCLASS) unless ctx.node_instance_methods.detect {|sexp_item| sexp_item.name.to_s == method_sexp.name_without_bang }
-          end
+          next unless method_sexp.ends_with_bang?
+          next if method_or_attr_method_exist?(ctx, method_sexp)
+          SmellWarning.new(SMELL_CLASS, ctx.full_name, [ctx.exp.line],
+                           %Q(has prima donna method `#{method_sexp.name}`),
+                           @source, SMELL_SUBCLASS)
         end.compact
+      end
+
+      private
+
+      def method_or_attr_method_exist?(ctx, method_sexp)
+        method_without_bang_exist?(ctx, method_sexp) ||
+          attr_call_exist?(ctx, method_sexp)
+      end
+
+      def method_without_bang_exist?(ctx, method_sexp)
+        ctx.node_instance_methods.find do |sexp_item|
+          sexp_item.name.to_s == method_sexp.name_without_bang
+        end
+      end
+
+      def attr_call_exist?(ctx, method_sexp)
+        ctx.local_nodes(:call).find do |sexp_item|
+          [:attr_reader, :attr_accessor].include?(sexp_item.method_name) &&
+            sexp_item.arg_names.first.to_s == method_sexp.name_without_bang
+        end
       end
     end
   end
