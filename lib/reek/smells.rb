e798f34646b363adc556506e76ac65f04604469c
Timo Rößner <timo.roessner@googlemail.com>
2015-02-15 22:45:30 +0100

    Refactor smells module.

--- lib/reek/smells.rb
+++ lib/reek/smells.rb
@@ -1,28 +1,6 @@
-require 'reek/smells/attribute'
-require 'reek/smells/boolean_parameter'
-require 'reek/smells/class_variable'
-require 'reek/smells/control_parameter'
-require 'reek/smells/data_clump'
-require 'reek/smells/duplicate_method_call'
-require 'reek/smells/feature_envy'
-require 'reek/smells/irresponsible_module'
-require 'reek/smells/long_parameter_list'
-require 'reek/smells/long_yield_list'
-require 'reek/smells/module_initialize'
-require 'reek/smells/nested_iterators'
-require 'reek/smells/nil_check'
-require 'reek/smells/prima_donna_method'
-require 'reek/smells/repeated_conditional'
-require 'reek/smells/too_many_instance_variables'
-require 'reek/smells/too_many_methods'
-require 'reek/smells/too_many_statements'
-require 'reek/smells/uncommunicative_method_name'
-require 'reek/smells/uncommunicative_module_name'
-require 'reek/smells/uncommunicative_parameter_name'
-require 'reek/smells/uncommunicative_variable_name'
-require 'reek/smells/unused_parameters'
-require 'reek/smells/utility_function'
-# SMELL: Duplication -- all these should be found automagically
+require 'require_all'
+
+require_rel 'smells'
 
 module Reek
   #
