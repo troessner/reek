19d4b909141bbfd6fa4d8e3cfe1d0e2179f6e972
Timo Rößner <timo.roessner@googlemail.com>
2015-06-28 00:01:10 +0200

    Make directories excludable via configuration.

--- spec/reek/configuration/app_configuration_spec.rb
+++ spec/reek/configuration/app_configuration_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../lib/reek/configuration/app_configuration'
 require_relative '../../../lib/reek/smells/smell_repository'
 
 RSpec.describe Reek::Configuration::AppConfiguration do
-  let(:sample_configuration_path) { 'spec/samples/simple_configuration.reek' }
+  let(:sample_configuration_path) { 'spec/samples/configuration/simple_configuration.reek' }
   let(:sample_configuration_loaded) do
     {
       'UncommunicativeVariableName' => { 'enabled' => false },
@@ -12,32 +12,32 @@ RSpec.describe Reek::Configuration::AppConfiguration do
   end
   let(:default_configuration) { Hash.new }
 
-  after(:each) { Reek::Configuration::AppConfiguration.reset }
+  after(:each) { described_class.reset }
 
   describe '.initialize_with' do
     it 'loads a configuration file if it can find one' do
       finder = Reek::Configuration::ConfigurationFileFinder
       allow(finder).to receive(:find).and_return sample_configuration_path
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(default_configuration)
+      expect(described_class.configuration).to eq(default_configuration)
 
-      Reek::Configuration::AppConfiguration.initialize_with(nil)
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(sample_configuration_loaded)
+      described_class.initialize_with(nil)
+      expect(described_class.configuration).to eq(sample_configuration_loaded)
     end
 
     it 'leaves the default configuration untouched if it can\'t find one' do
       allow(Reek::Configuration::ConfigurationFileFinder).to receive(:find).and_return nil
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(default_configuration)
+      expect(described_class.configuration).to eq(default_configuration)
 
-      Reek::Configuration::AppConfiguration.initialize_with(nil)
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(default_configuration)
+      described_class.initialize_with(nil)
+      expect(described_class.configuration).to eq(default_configuration)
     end
   end
 
   describe '.configure_smell_repository' do
     it 'should configure a given smell_repository' do
-      Reek::Configuration::AppConfiguration.load_from_file(sample_configuration_path)
+      described_class.load_from_file(sample_configuration_path)
       smell_repository = Reek::Smells::SmellRepository.new('def m; end')
-      Reek::Configuration::AppConfiguration.configure_smell_repository smell_repository
+      described_class.configure_smell_repository smell_repository
 
       expect(smell_repository.detectors[Reek::Smells::DataClump]).to be_enabled
       expect(smell_repository.detectors[Reek::Smells::UncommunicativeVariableName]).
@@ -46,20 +46,33 @@ RSpec.describe Reek::Configuration::AppConfiguration do
     end
   end
 
+  describe '.exclude_paths' do
+    let(:config_path) { 'spec/samples/configuration/with_excluded_paths.reek' }
+
+    it 'should return all paths to exclude' do
+      with_test_config(config_path) do
+        expect(described_class.exclude_paths).to eq [
+          'spec/samples/source_with_exclude_paths/ignore_me',
+          'spec/samples/source_with_exclude_paths/nested/ignore_me_as_well'
+        ]
+      end
+    end
+  end
+
   describe '.load_from_file' do
     it 'loads the configuration from given file' do
-      Reek::Configuration::AppConfiguration.load_from_file(sample_configuration_path)
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(sample_configuration_loaded)
+      described_class.load_from_file(sample_configuration_path)
+      expect(described_class.configuration).to eq(sample_configuration_loaded)
     end
   end
 
   describe '.reset' do
     it 'resets the configuration' do
-      Reek::Configuration::AppConfiguration.load_from_file(sample_configuration_path)
+      described_class.load_from_file(sample_configuration_path)
 
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(sample_configuration_loaded)
-      Reek::Configuration::AppConfiguration.reset
-      expect(Reek::Configuration::AppConfiguration.configuration).to eq(default_configuration)
+      expect(described_class.configuration).to eq(sample_configuration_loaded)
+      described_class.reset
+      expect(described_class.configuration).to eq(default_configuration)
     end
   end
 end
