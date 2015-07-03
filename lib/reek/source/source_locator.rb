da1926c8068da67b4f18d2169abe7b01dcfabf0b
Timo Rößner <timo.roessner@googlemail.com>
2015-07-03 16:14:00 +0200

    Only use ruby source files.

--- lib/reek/source/source_locator.rb
+++ lib/reek/source/source_locator.rb
@@ -31,16 +31,16 @@ module Reek
           Find.find(given_path) do |path|
             pathname = Pathname.new(path)
             if pathname.directory?
-              exclude_path?(pathname) || hidden_directory?(pathname) ? Find.prune : next
+              ignore_path?(pathname) ? Find.prune : next
             else
-              relevant_paths << pathname
+              relevant_paths << pathname if ruby_file?(pathname)
             end
           end
         end
         relevant_paths.flatten.sort
       end
 
-      def exclude_path?(pathname)
+      def path_excluded?(pathname)
         Configuration::AppConfiguration.exclude_paths.include? pathname.to_s
       end
 
@@ -54,6 +54,14 @@ module Reek
 
       def hidden_directory?(pathname)
         pathname.basename.to_s.start_with? '.'
+      end
+
+      def ignore_path?(pathname)
+        path_excluded?(pathname) || hidden_directory?(pathname)
+      end
+
+      def ruby_file?(pathname)
+        pathname.to_s.end_with?('.rb')
       end
     end
   end
