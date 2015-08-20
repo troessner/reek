53137689bdaf75e0c7cdbcf4ff450a950dc56af2
Matijs van Zuijlen <matijs@matijs.net>
2015-08-20 21:34:48 +0200

    Simplify CodeComment implementation
    - Removes undocumented support for underscored smell names.
    - Improves smell suppression documentation.

--- lib/reek/code_comment.rb
+++ lib/reek/code_comment.rb
@@ -14,30 +14,25 @@ module Reek
     CONFIG_REGEX = /:reek:(\w+)(:\s*\{.*?\})?/
 
     def initialize(text)
+      @config = Hash.new { |hash, key| hash[key] = {} }
       @text = text.gsub(CONFIG_REGEX) do
-        add_to_config($1, $2)
+        @config.merge! add_to_config($1, $2)
         ''
       end.gsub(/#/, '').gsub(/\n/, '').strip
     end
 
-    def config
-      @config ||= Hash.new { |hash, key| hash[key] = {} }
-    end
+    attr_reader :config
 
     def descriptive?
       text.split(/\s+/).length >= 2
     end
 
-    protected
+    private
 
     def add_to_config(smell, options)
       options ||= ': { enabled: false }'
-      config.merge! YAML.load(smell.gsub(/(?:^|_)(.)/) { $1.upcase } + options)
-      # TODO: extend this to all configs -------------------^
-      # TODO: extend to allow configuration of whole smell class, not just subclass
+      YAML.load(smell + options)
     end
-
-    private
 
     private_attr_reader :text
   end
