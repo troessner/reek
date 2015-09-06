bc40acc1dfa94f538a4f3cc1256d3d3a2cd1be06
Matijs van Zuijlen <matijs@matijs.net>
2015-09-06 21:29:42 +0200

    Finalize preliminary implementation
    Only adds section if needed, only marks private API in output.

--- docs/templates/default/docstring/setup.rb
+++ docs/templates/default/docstring/setup.rb
@@ -1,21 +1,42 @@
 def init
   super
-  if sections.first
-    sections.first.place(:api_marker).before(:private)
-  else
-    sections :index, [:api_marker]
+  if show_api_marker_section?
+    if sections.first
+      sections.first.place(:api_marker).before(:private)
+    else
+      sections :index, [:api_marker]
+    end
   end
 end
 
 def api_marker
-  api_text = object.has_tag?(:api) && object.tag(:api).text
-  api_text = 'public' if object.has_tag?(:public)
+  return if object.type == :root
   case api_text
   when 'public'
-    erb(:public_api_marker)
+    #erb(:public_api_marker)
   when 'private'
     # Let section 'private' handle this.
   else
     erb(:private)
+  end
+end
+
+private
+
+def api_text
+  api_text = object.has_tag?(:api) && object.tag(:api).text
+  api_text = 'public' if object.has_tag?(:public)
+  return api_text
+end
+
+def show_api_marker_section?
+  return false if object.type == :root
+  case api_text
+  when 'public'
+    false
+  when 'private'
+    false
+  else
+    true
   end
 end
