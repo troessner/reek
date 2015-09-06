2efc6609f8d2b87d91c3e981045e6cd8465245ab
Matijs van Zuijlen <matijs@matijs.net>
2015-09-06 21:39:28 +0200

    Please RuboCop

--- docs/templates/default/docstring/setup.rb
+++ docs/templates/default/docstring/setup.rb
@@ -1,11 +1,10 @@
 def init
   super
-  if show_api_marker_section?
-    if sections.first
-      sections.first.place(:api_marker).before(:private)
-    else
-      sections :index, [:api_marker]
-    end
+  return unless show_api_marker_section?
+  if sections.first
+    sections.first.place(:api_marker).before(:private)
+  else
+    sections :index, [:api_marker]
   end
 end
 
@@ -13,7 +12,7 @@ def api_marker
   return if object.type == :root
   case api_text
   when 'public'
-    #erb(:public_api_marker)
+    # erb(:public_api_marker)
   when 'private'
     # Let section 'private' handle this.
   else
@@ -26,7 +25,7 @@ private
 def api_text
   api_text = object.has_tag?(:api) && object.tag(:api).text
   api_text = 'public' if object.has_tag?(:public)
-  return api_text
+  api_text
 end
 
 def show_api_marker_section?
