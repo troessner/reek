6a1f7efb37ce5c54ae01c63f221916c41ecc0b26
Timo Rößner <timo.roessner@googlemail.com>
2015-10-28 10:02:11 +0100

    Fix Ataru travis failures.

---
+++ ataru_setup.rb
@@ -0,0 +1,13 @@
+require 'reek'
+require 'reek/smells'
+
+# Ataru setup module.
+module Setup
+  def setup
+    # Run before every snippet.
+  end
+
+  def teardown
+    # Run after every snippet.
+  end
+end
