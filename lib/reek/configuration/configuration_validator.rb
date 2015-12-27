27a02da5157886de8e5254eadd912d5e4a1a211d
Timo Rößner <timo.roessner@googlemail.com>
2015-12-27 21:44:01 +0100

    Don't raise on missing directory for directive.

--- lib/reek/configuration/configuration_validator.rb
+++ lib/reek/configuration/configuration_validator.rb
@@ -30,22 +30,13 @@ module Reek
         end
       end
 
-      def error_message_for_missing_directory(pathname)
-        "Configuration error: Directory `#{pathname}` does not exist"
-      end
-
       def error_message_for_file_given(pathname)
         "Configuration error: `#{pathname}` is supposed to be a directory but is a file"
       end
 
-      def validate_directory(pathname)
-        abort(error_message_for_missing_directory(pathname)) unless pathname.exist?
-        abort(error_message_for_file_given(pathname)) if pathname.file?
-      end
-
       def with_valid_directory(path)
         directory = Pathname.new path.to_s.chomp('/')
-        validate_directory directory
+        abort(error_message_for_file_given(directory)) if directory.file?
         yield directory if block_given?
       end
     end
