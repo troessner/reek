16d05b3edae2c9f90f39de4b56981a5b494c18ad
Matijs van Zuijlen <matijs@matijs.net>
2016-02-07 13:15:20 +0100

    Bring Application, Task and ReekCommand under test
    Refactors Application and ReekCommand to improve testability and
    decrease complex coupling.

---
+++ spec/reek/cli/reek_command_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../../spec_helper'
+require_lib 'reek/cli/reek_command'
+require_lib 'reek/cli/options'
+require_lib 'reek/cli/option_interpreter'
+
+RSpec.describe Reek::CLI::ReekCommand do
+  describe '#execute' do
+    let(:options) { Reek::CLI::Options.new [] }
+    let(:option_interpreter) { Reek::CLI::OptionInterpreter.new(options) }
+
+    let(:reporter) { double 'reporter' }
+    let(:app) { double 'app' }
+
+    let(:command) { described_class.new option_interpreter }
+
+    before do
+      allow(option_interpreter).to receive(:reporter).and_return reporter
+      allow(reporter).to receive(:show)
+    end
+
+    context 'when no smells are found' do
+      before do
+        allow(option_interpreter).to receive(:sources).and_return []
+        allow(reporter).to receive(:smells?).and_return false
+      end
+
+      it 'returns a success code' do
+        result = command.execute app
+        expect(result).to eq Reek::CLI::Options::DEFAULT_SUCCESS_EXIT_CODE
+      end
+    end
+
+    context 'when smells are found' do
+      before do
+        allow(option_interpreter).to receive(:sources).and_return []
+        allow(reporter).to receive(:smells?).and_return true
+      end
+
+      it 'returns a failure code' do
+        result = command.execute app
+        expect(result).to eq Reek::CLI::Options::DEFAULT_FAILURE_EXIT_CODE
+      end
+    end
+  end
+end
