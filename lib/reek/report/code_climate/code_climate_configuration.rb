1bc7eaa16c7cff41ef7cd56e73d491420370e8a4
Max Jacobson <max@hardscrabble.net>
2016-10-13 14:44:51 -0400

    Add missing Code Climate issue configuration
    Also added a test to help make sure this stays up-to-date.
    
    Seems like there is some duplication here. I'm wondering if it should
    read this in from the docs folder?

---
+++ lib/reek/report/code_climate/code_climate_configuration.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+module Reek
+  module Report
+    # loads the smell type metadata to present in Code Climate
+    module CodeClimateConfiguration
+      def self.load
+        config_file = File.expand_path('../code_climate_configuration.yml', __FILE__)
+        YAML.load_file config_file
+      end
+    end
+  end
+end
---
+++ spec/reek/report/code_climate/code_climate_configuration_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../../spec_helper'
+require_lib 'reek/report/code_climate/code_climate_configuration'
+
+RSpec.describe Reek::Report::CodeClimateConfiguration do
+  yml = described_class.load
+  smell_types = Reek::SmellDetectors::BaseDetector.descendants.map do |descendant|
+    descendant.name.demodulize
+  end
+
+  smell_types.each do |name|
+    config = yml.fetch(name)
+    it "provides remediation_points for #{name}" do
+      expect(config['remediation_points']).to be_a Fixnum
+    end
+
+    it "provides content for #{name}" do
+      expect(config['content']).to be_a String
+    end
+  end
+
+  it 'does not include extraneous configuration' do
+    expect(smell_types).to match_array(yml.keys)
+  end
+end
