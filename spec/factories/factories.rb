72904eb0733824b02be0afbddfb852a9783e8d85
Matijs van Zuijlen <matijs@matijs.net>
2017-11-25 14:54:49 +0100

    Remove unused factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,17 +4,6 @@ require_relative '../../lib/reek/smell_warning'
 require_relative '../../lib/reek/cli/options'
 
 FactoryBot.define do
-  factory :method_context, class: Reek::Context::MethodContext do
-    skip_create
-    transient do
-      source 'def foo; end'
-    end
-
-    initialize_with do
-      new(nil, Reek::Source::SourceCode.from(source).syntax_tree)
-    end
-  end
-
   factory :smell_detector, class: Reek::SmellDetectors::BaseDetector do
     skip_create
     transient do
