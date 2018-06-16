f078a8499c4e891f78af1ca956b28ebbdc1df80b
Matijs van Zuijlen <matijs@matijs.net>
2018-06-16 08:06:35 +0200

    Document and improve DocumentationLink

--- lib/reek/documentation_link.rb
+++ lib/reek/documentation_link.rb
@@ -7,12 +7,22 @@ module Reek
 
     module_function
 
-    def build(item)
-      Kernel.format(HELP_LINK_TEMPLATE, version: Version::STRING, item: item_name_to_param(item))
+    # Build link to the documentation about the given subject for the current
+    # version of Reek. The subject can be either a smell type like
+    # 'FeatureEnvy' or a general subject like 'Rake Task'.
+    #
+    # @param subject [String]
+    # @return [String] - the full URL for the relevant documentation
+    def build(subject)
+      Kernel.format(HELP_LINK_TEMPLATE, version: Version::STRING, item: name_to_param(subject))
     end
 
-    def item_name_to_param(name)
-      name.split(/ *(?=[A-Z])/).join('-')
+    # Convert the given subject name to a form that is acceptable in a URL.
+    def name_to_param(name)
+      # Splits the subject on the start of capitalized words, optionally
+      # preceded by a space. The space is discarded, the start of the word is
+      # not.
+      name.split(/ *(?=[A-Z][a-z])/).join('-')
     end
   end
 end
